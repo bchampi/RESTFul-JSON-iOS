c3497437465156c990dea72df091ac6ae71b6bf6
Mac 17 <brayan.champi@tecsup.edu.pe>
2021-06-10 14:46:40 -0500

    Initial Commit

---
+++ RestFulJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestFulJSON
+//
+//  Created by Mac 17 on 6/10/21.
+//  Copyright © 2021 deah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
